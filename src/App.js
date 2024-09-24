bac322132fdb0f9f91c1ae02112c10d67398c00c
Siddhanta Sahoo <siddhanta.sahoo@oasystspl.com>
2024-09-24 18:12:52 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import DataTable from './DataTable';
+const data = [
+  { id: 1, name: 'Rashmi', age: 41 },
+  { id: 2, name: 'Ranjan', age: 40 },
+  { id: 3, name: 'Ranasingh', age: 39 },
+];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Table</h1>
+      <DataTable data={data} />
     </div>
   );
 }
 
 export default App;
+
